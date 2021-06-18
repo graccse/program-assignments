7c9f3cab6e018ed2475f6fbbd65b894fcf278ae4
graccse <85902165+graccse@users.noreply.github.com>
2021-06-18 12:36:56 -0500

    Add files via upload

---
+++ list_practice.py
@@ -0,0 +1,19 @@
+empty_list = []
+
+city_list = ["Oakland", "Atlanta", "New York City", "Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+print(city_list)
+print(city_list[3])
+print(city_list[4])
+print(city_list[8])
+
+three_cities = city_list[0:3]
+print(three_cities)
+
+city_list[0] = "San Francisco"
+city_list[2] = "Brooklyn"
+city_list[7] = "Hollywood"
+city_list[5] = "Tampa"
+print (city_list)
+
+dessert_list = ["Ice Cream", "Gelato", "Jello","Cake","Pie","Tarts", "Cookies", "Macarons", "Yogurt","Donuts"]
+
